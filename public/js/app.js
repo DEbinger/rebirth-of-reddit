fdf00607b96ae7302c98ada9a944bb170095374d
David <davidebinger888@gmail.com>
2016-12-20 21:47:30 -1000

    changed img element to a div element, images posting

--- public/js/app.js
+++ public/js/app.js
@@ -4,49 +4,52 @@
 
 function reqListener(){
 
-  var pug = JSON.parse(this.responseText);
+  let pug = JSON.parse(this.responseText);
 
-  for (var i = 0; i < 10; i++){
+  for (let i = 0; i < 10; i++){
 
-    var articleContainer = document.createElement('div');
+    let articleContainer = document.createElement('div');
     articleContainer.className = 'article-container';
     document.getElementById('content-container').appendChild(articleContainer);
 
-    var pugPic = document.createElement('img');
+    let pugPic = document.createElement('div');
+    pugPic.id = pugPic;
     pugPic.className = "image-card";
-    pugPic.setAttribute('src', pug.data.children[i].data.preview.images[0].source.url);
+    let imageUrl= pug.data.children[i].data.preview.images[0].source.url;
+    //pugPic.setAttribute('src', pug.data.children[i].data.preview.images[0].source.url);
+    pugPic.style.backgroundImage = `url('${imageUrl}')`;
     document.getElementsByClassName('article-container')[0].appendChild(pugPic);
 
-    var titleDivCreate = document.createElement('div');
+    let titleDivCreate = document.createElement('div');
     titleDivCreate.className = 'headline';
-    var pugTitle = document.createTextNode(pug.data.children[i].data.title);
+    let pugTitle = document.createTextNode(pug.data.children[i].data.title);
     titleDivCreate.appendChild(pugTitle);
     document.getElementsByClassName('article-container')[0].appendChild(titleDivCreate);
     //document.getElementById().innerHTML
 
-    var authorDivCreate = document.createElement('div');
+    let authorDivCreate = document.createElement('div');
     authorDivCreate.className = 'author';
-    var author = document.createTextNode(pug.data.children[i].data.author);
+    let author = document.createTextNode(pug.data.children[i].data.author);
     authorDivCreate.appendChild(author);
     document.getElementsByClassName('article-container')[0].appendChild(authorDivCreate);
 
-    //var utc = utc_created;
-    var createdDateDivCreate = document.createElement('div');
+    //let utc = utc_created;
+    let createdDateDivCreate = document.createElement('div');
     createdDateDivCreate.className = 'createdDate';
-    var createdDate = document.createTextNode(pug.data.children[i].data.utc);
+    let createdDate = document.createTextNode(pug.data.children[i].data.utc);
     createdDateDivCreate.appendChild(createdDate);
     document.getElementsByClassName('article-container')[0].appendChild(createdDateDivCreate);
-    //var dateString = moment(utc).fromNow();
+    //let dateString = moment(utc).fromNow();
 
-    var viewDivCreate = document.createElement('div');
+    let viewDivCreate = document.createElement('div');
     viewDivCreate.className = 'views';
-    var views = document.createTextNode(pug.data.children[i].data.num_comments);
+    let views = document.createTextNode(pug.data.children[i].data.num_comments);
     viewDivCreate.appendChild(views);
     document.getElementsByClassName('article-container')[0].appendChild(viewDivCreate);
 
-    var scoreDivCreate = document.createElement('div');
+    let scoreDivCreate = document.createElement('div');
     scoreDivCreate.className = 'scores';
-    var scores = document.createTextNode(pug.data.children[i].data.score);
+    let scores = document.createTextNode(pug.data.children[i].data.score);
     scoreDivCreate.appendChild(scores);
     document.getElementsByClassName('article-container')[0].appendChild(scoreDivCreate);
 
@@ -55,7 +58,7 @@ function reqListener(){
 }
 
 
-var oReq = new XMLHttpRequest();
+let oReq = new XMLHttpRequest();
 oReq.addEventListener('load', reqListener);
 oReq.open('GET', 'https://www.reddit.com/r/pug.json');
 oReq.send();
