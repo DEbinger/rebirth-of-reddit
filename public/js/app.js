84a32efafdf9d11dbafda246e8c8f9d4f504d0d4
David <davidebinger888@gmail.com>
2016-12-22 22:27:16 -1000

    MVP Large size, need to refactor for media queries

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ function reqListener(){
 
   let pug = JSON.parse(this.responseText);
 
-  for (let i = 0; i < 10; i++){
+  for (let i = 0; i < 24; i++){
 
     let articleContainer = document.createElement('div');
     articleContainer.className = 'article-container';
@@ -16,42 +16,40 @@ function reqListener(){
     pugPic.id = pugPic;
     pugPic.className = "image-card";
     let imageUrl= pug.data.children[i].data.preview.images[0].source.url;
-    //pugPic.setAttribute('src', pug.data.children[i].data.preview.images[0].source.url);
     pugPic.style.backgroundImage = `url('${imageUrl}')`;
-    document.getElementsByClassName('article-container')[0].appendChild(pugPic);
+    articleContainer.appendChild(pugPic);
 
     let titleDivCreate = document.createElement('div');
     titleDivCreate.className = 'headline';
     let pugTitle = document.createTextNode(pug.data.children[i].data.title);
     titleDivCreate.appendChild(pugTitle);
-    document.getElementsByClassName('article-container')[0].appendChild(titleDivCreate);
-    //document.getElementById().innerHTML
+    articleContainer.appendChild(titleDivCreate);
 
     let authorDivCreate = document.createElement('div');
     authorDivCreate.className = 'author';
     let author = document.createTextNode(pug.data.children[i].data.author);
     authorDivCreate.appendChild(author);
-    document.getElementsByClassName('article-container')[0].appendChild(authorDivCreate);
+    articleContainer.appendChild(authorDivCreate);
 
-    //let utc = utc_created;
-    let createdDateDivCreate = document.createElement('div');
-    createdDateDivCreate.className = 'createdDate';
-    let createdDate = document.createTextNode(pug.data.children[i].data.utc);
-    createdDateDivCreate.appendChild(createdDate);
-    document.getElementsByClassName('article-container')[0].appendChild(createdDateDivCreate);
-    //let dateString = moment(utc).fromNow();
+    let dateDivCreate = document.createElement('div');
+    dateDivCreate.className = 'time-ago';
+    var date = moment.unix(pug.data.children[i].data.created_utc);
+    var today = new Date();
+    var duration = document.createTextNode(date.from(today));
+    dateDivCreate.innerHTML = duration;
+    articleContainer.appendChild(duration);
 
     let viewDivCreate = document.createElement('div');
     viewDivCreate.className = 'views';
     let views = document.createTextNode(pug.data.children[i].data.num_comments);
     viewDivCreate.appendChild(views);
-    document.getElementsByClassName('article-container')[0].appendChild(viewDivCreate);
+    articleContainer.appendChild(viewDivCreate);
 
     let scoreDivCreate = document.createElement('div');
     scoreDivCreate.className = 'scores';
     let scores = document.createTextNode(pug.data.children[i].data.score);
     scoreDivCreate.appendChild(scores);
-    document.getElementsByClassName('article-container')[0].appendChild(scoreDivCreate);
+    articleContainer.appendChild(scoreDivCreate);
 
   }
 
